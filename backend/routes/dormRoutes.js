4b551f5c182227d7ae61cba6ec9d968b39468818
Ezedin Aliyi <ezedinaliyi38@gmail.com>
2026-02-02 20:16:42 +0300

    Add manager statistics endpoint for system-wide analytics

--- backend/routes/dormRoutes.js
+++ backend/routes/dormRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getRooms, getRoomById, createRoom, updateRoom, deleteRoom, assignStudentToRoom } = require('../controllers/dormController');
+const { getRooms, getRoomById, createRoom, updateRoom, deleteRoom, assignStudentToRoom, autoAllocate, getStatistics } = require('../controllers/dormController');
 
 router.route('/').get(getRooms).post(createRoom);
+router.route('/allocate').post(autoAllocate);
+router.route('/statistics').get(getStatistics);
 router.route('/:id').get(getRoomById).put(updateRoom).delete(deleteRoom);
 router.route('/:id/assign').post(assignStudentToRoom);
 
