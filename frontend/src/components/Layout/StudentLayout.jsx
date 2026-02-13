98e6048fe37557aeebea0829ede27c03d038b13f
Moi-29 <moibonberhanu6@gmail.com>
2026-02-13 23:17:41 +0300

    error fixed

--- frontend/src/components/Layout/StudentLayout.jsx
+++ frontend/src/components/Layout/StudentLayout.jsx
@@ -126,7 +126,15 @@ const StudentLayout = () => {
                     </button>
 
                     {/* Google Translate Widget */}
-                    <div id="google_translate_element" style={{ flexShrink: 0 }}></div>
+                    <div 
+                        id="google_translate_element" 
+                        style={{ 
+                            flexShrink: 0,
+                            display: 'flex',
+                            alignItems: 'center',
+                            minHeight: '44px'
+                        }}
+                    ></div>
                 </div>
 
                 {/* Center - University Name (Hidden on small mobile) */}
